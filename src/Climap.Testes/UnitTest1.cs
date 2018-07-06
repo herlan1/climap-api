96a5439663cedfd707733af881e0cc0c42a67407
Herlan Rodrigues Siqueira <herlan.siqueira@inventsoftware.com.br>
2018-07-06 07:47:26 -0300

    Start do Projeto

---
+++ src/Climap.Testes/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Climap.Testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
